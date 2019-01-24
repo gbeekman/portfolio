561da75b48b61c224b17e5e608088fed00d93fa8
Graham Beekman <grahambeekman@gmail.com>
2019-01-24 14:56:27 +0700

    Implemented devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :my_portfolios, except: [:show]
   get 'angular-items', to: 'my_portfolios#angular'
   get 'portfolio/:id', to: 'my_portfolios#show', as: 'portfolio_show'
